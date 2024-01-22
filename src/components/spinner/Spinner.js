d9015b90547205bcab7f3edebc519cb7d6a5128b
JSDEV <vlady.dev127@gmail.com>
2024-01-22 01:16:56 -0600

    fixed spinner issue

--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -1,14 +1,12 @@
-import { BoxesLoader } from "react-awesome-loaders";
-
 export default function Spinner({position}) {
   return (
     <div className={`${position ? position : 'fixed'} top-[calc(50%-48px)] left-[calc(50%-48px)] z-[999]`}>
-      <BoxesLoader
+      {/* <BoxesLoader
         boxColor={"#4A9F2E"}
         // style={{ marginBottom: "20px" }}
         desktopSize={"96px"}
         mobileSize={"96px"}
-      />
+      /> */}
     </div>
   );
 }
